af44045b0a610f654570aeb39de99fc84e9b63bd
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-04-02 19:11:27 +0700

    Initialize project to repository.

---
+++ app/src/main/java/com/taufik/themovieshow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taufik.themovieshow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
