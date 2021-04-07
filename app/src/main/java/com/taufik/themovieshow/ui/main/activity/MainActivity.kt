a116e24f32d4dd49a369cfbc6f37e2682d49aff2
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-04-07 14:03:02 +0700

    Adding navigation component in main activity.

--- app/src/main/java/com/taufik/themovieshow/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/activity/MainActivity.kt
@@ -2,11 +2,19 @@ package com.taufik.themovieshow.ui.main.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.taufik.themovieshow.R
 import com.taufik.themovieshow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +23,27 @@ class MainActivity : AppCompatActivity() {
 
         initActionBar()
 
-
+        setNavHost()
     }
 
     private fun initActionBar() {
         supportActionBar?.elevation = 0F
     }
 
+    private fun setNavHost() {
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_movie,
+            R.id.nav_tv_show
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.apply {
+            navBottom.setupWithNavController(navController)
+        }
+    }
 }
