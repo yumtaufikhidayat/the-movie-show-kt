327fa8f13fa4aec4c0fb9e2c57027859c0635f7d
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-04-15 19:20:12 +0700

    Removing unused imports and search favorite edit text.

--- app/src/main/java/com/taufik/themovieshow/ui/feature/tvshow/data/local/FavoriteTvShowDao.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/feature/tvshow/data/local/FavoriteTvShowDao.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import com.taufik.themovieshow.ui.feature.movie.data.local.FavoriteMovie
 
 @Dao
 interface FavoriteTvShowDao {
