9a6064f6042ed52a46dfa11ef30e6322863e23b6
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-08-31 08:53:09 +0700

    Fix nullable image url.

--- app/src/main/java/com/taufik/themovieshow/utils/Common.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/Common.kt
@@ -7,7 +7,7 @@ import com.taufik.themovieshow.R
 import com.taufik.themovieshow.api.UrlEndpoint
 import kotlin.math.roundToInt
 
-fun ImageView.loadImage(url: String) {
+fun ImageView.loadImage(url: String?) {
     Glide.with(this.context)
         .load(UrlEndpoint.IMAGE_URL + url)
         .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error))
