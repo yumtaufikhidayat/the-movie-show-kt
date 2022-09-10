b8e5d5e10ddd168c9149bb2cc2c2397f9186b587
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-09-11 06:41:09 +0700

    Add okHttp client and interceptor.

--- app/src/main/java/com/taufik/themovieshow/data/api/ApiClient.kt
+++ app/src/main/java/com/taufik/themovieshow/data/api/ApiClient.kt
@@ -1,11 +1,32 @@
 package com.taufik.themovieshow.data.api
 
+import com.taufik.themovieshow.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiClient {
+
+    private const val timeOutTime = 30L
+
+    private val interceptor = if (BuildConfig.DEBUG) {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    } else {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .connectTimeout(timeOutTime, TimeUnit.SECONDS)
+        .writeTimeout(timeOutTime, TimeUnit.SECONDS)
+        .readTimeout(timeOutTime, TimeUnit.SECONDS)
+        .build()
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(UrlEndpoint.BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
