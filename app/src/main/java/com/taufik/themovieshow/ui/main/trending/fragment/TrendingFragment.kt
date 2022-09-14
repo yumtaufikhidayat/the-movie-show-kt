f407ac43fd11d5f956bb2c0c7f293f2ee6d325c2
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-09-15 06:03:48 +0700

    Implement handling on back pressed on fragment.

--- app/src/main/java/com/taufik/themovieshow/ui/main/trending/fragment/TrendingFragment.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/trending/fragment/TrendingFragment.kt
@@ -1,20 +1,28 @@
 package com.taufik.themovieshow.ui.main.trending.fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
-import com.taufik.themovieshow.R
 import com.taufik.themovieshow.databinding.FragmentTrendingBinding
 import com.taufik.themovieshow.ui.main.trending.adapter.TrendingPagerAdapter
+import es.dmoral.toasty.Toasty
+
 
 class TrendingFragment : Fragment() {
 
     private var _binding: FragmentTrendingBinding? = null
     private val binding get() = _binding!!
+
+    private val delayTime = 2000L
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
@@ -24,12 +32,13 @@ class TrendingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, backPressedCallback)
         setToolbar()
         setTabLayout()
     }
 
     private fun setToolbar() = with(binding) {
-        toolbarTrending.tvToolbar.text = getString(R.string.icTrending)
+        toolbarTrending.tvToolbar.text = getString(com.taufik.themovieshow.R.string.icTrending)
     }
 
     private fun setTabLayout() = with(binding) {
@@ -45,8 +54,25 @@ class TrendingFragment : Fragment() {
         _binding = null
     }
 
+    private val backPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (doubleBackToExitPressedOnce) {
+                requireActivity().finish()
+                return
+            }
+
+            doubleBackToExitPressedOnce = true
+            Toasty.info(requireContext(), "Press back again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler(Looper.getMainLooper())
+                .postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, delayTime)
+        }
+    }
+
     companion object {
         @StringRes
-        private val tabsTitle = intArrayOf(R.string.tvMovies, R.string.tvShows)
+        private val tabsTitle = intArrayOf(com.taufik.themovieshow.R.string.tvMovies, com.taufik.themovieshow.R.string.tvShows)
     }
 }
