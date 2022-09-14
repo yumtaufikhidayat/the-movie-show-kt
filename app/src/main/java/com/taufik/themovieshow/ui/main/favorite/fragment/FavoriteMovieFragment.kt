04fe53147122f54289c71cbac93a107f8425071d
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-09-14 07:21:53 +0700

    Implement search for movies favorite.

--- app/src/main/java/com/taufik/themovieshow/ui/main/favorite/fragment/FavoriteMovieFragment.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/favorite/fragment/FavoriteMovieFragment.kt
@@ -1,9 +1,14 @@
 package com.taufik.themovieshow.ui.main.favorite.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -32,8 +37,10 @@ class FavoriteMovieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         setAdapter()
         getFavoriteMovie()
+        searchData()
     }
 
     private fun setAdapter() = with(binding) {
@@ -46,9 +53,23 @@ class FavoriteMovieFragment : Fragment() {
 
     private fun getFavoriteMovie() {
         viewModel.getFavoriteMovies()?.observe(viewLifecycleOwner) {
-            if (it != null) {
-                movieAdapter.submitList(mapList(it))
-            }
+            if (it != null && it.isNotEmpty()) movieAdapter.setData(mapList(it))
+        }
+    }
+
+    private fun searchData() {
+        binding.etSearch.apply {
+            setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    hideKeyboard()
+                    return@OnEditorActionListener true
+                }
+                false
+            })
+
+            addTextChangedListener(afterTextChanged = { p0 ->
+                movieAdapter.filter.filter(p0.toString())
+            })
         }
     }
 
@@ -66,6 +87,12 @@ class FavoriteMovieFragment : Fragment() {
         }
 
         return listMovies
+    }
+
+    private fun hideKeyboard() = with(binding) {
+        etSearch.clearFocus()
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(etSearch.windowToken, 0)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/taufik/themovieshow/ui/main/movie/adapter/MovieAdapter.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/movie/adapter/MovieAdapter.kt
@@ -3,6 +3,8 @@ package com.taufik.themovieshow.ui.main.movie.adapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -15,8 +17,16 @@ import com.taufik.themovieshow.utils.CommonDateFormatConstants
 import com.taufik.themovieshow.utils.convertDate
 import com.taufik.themovieshow.utils.loadImage
 import com.taufik.themovieshow.utils.toRating
+import java.util.*
 
-class MovieAdapter : ListAdapter<MovieMainResult, MovieAdapter.MovieViewHolder>(MovieDiffCallback){
+class MovieAdapter : ListAdapter<MovieMainResult, MovieAdapter.MovieViewHolder>(MovieDiffCallback), Filterable{
+
+    private var listMovies = listOf<MovieMainResult>()
+
+    fun setData(list: List<MovieMainResult>) {
+        this.listMovies = list
+        submitList(list)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         return MovieViewHolder(ItemsMoviesTvShowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -50,5 +60,31 @@ class MovieAdapter : ListAdapter<MovieMainResult, MovieAdapter.MovieViewHolder>(
     object MovieDiffCallback : DiffUtil.ItemCallback<MovieMainResult>(){
         override fun areItemsTheSame(oldItem: MovieMainResult, newItem: MovieMainResult): Boolean = oldItem.id == newItem.id
         override fun areContentsTheSame(oldItem: MovieMainResult, newItem: MovieMainResult): Boolean = oldItem == newItem
+    }
+
+    override fun getFilter(): Filter = searchFilter
+
+    private val searchFilter = object : Filter() {
+        override fun performFiltering(p0: CharSequence): FilterResults {
+            val filteredList = mutableListOf<MovieMainResult>()
+            if (p0.isEmpty()) {
+                filteredList.addAll(listMovies)
+            } else {
+                val filterPattern = p0.toString().lowercase(Locale.ROOT).trim()
+                listMovies.forEach { item ->
+                    if (item.title.lowercase().contains(filterPattern)) {
+                        filteredList.add(item)
+                    }
+                }
+            }
+
+            val result = FilterResults()
+            result.values = filteredList
+            return result
+        }
+
+        override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+            submitList(p1?.values as MutableList<MovieMainResult>?)
+        }
     }
 }
