0c84098c78de02d0252e83223fcee7e60bfea9b9
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-03-18 08:03:07 +0700

    Reformat code and optimize imports.

--- app/src/main/java/com/taufik/themovieshow/utils/UtilsData.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/UtilsData.kt
@@ -2,25 +2,23 @@ package com.taufik.themovieshow.utils
 
 import com.taufik.themovieshow.BuildConfig
 import com.taufik.themovieshow.R
+import com.taufik.themovieshow.model.response.about.About
 
 object UtilsData {
-
-    const val API_KEY = "c5d387e24584756e5a37eb08021e1840"
-
-    fun generateAboutAuthorData(): List<com.taufik.themovieshow.model.response.about.About> {
+    fun generateAboutAuthorData(): List<About> {
 
         return mutableListOf(
-            com.taufik.themovieshow.model.response.about.About(
+            About(
                 R.drawable.ic_outline_profile,
                 "Let's greet",
                 "Taufik Hidayat"
             ),
-            com.taufik.themovieshow.model.response.about.About(
+            About(
                 R.drawable.github,
                 "Fork on Github",
                 "Fork this app on Github"
             ),
-            com.taufik.themovieshow.model.response.about.About(
+            About(
                 R.drawable.ic_outline_email,
                 "Send an email",
                 "yumtaufikhidayat@gmail.com"
@@ -28,19 +26,19 @@ object UtilsData {
         )
     }
 
-    fun generateAboutApplicationData(): List<com.taufik.themovieshow.model.response.about.About> {
+    fun generateAboutApplicationData(): List<About> {
         return mutableListOf(
-            com.taufik.themovieshow.model.response.about.About(
+            About(
                 R.drawable.ic_update,
                 "Version",
                 BuildConfig.VERSION_NAME
             ),
-            com.taufik.themovieshow.model.response.about.About(
+            About(
                 R.drawable.ic_outline_rate,
                 "Rate this app",
                 "Like this app? Rate 5 stars"
             ),
-            com.taufik.themovieshow.model.response.about.About(
+            About(
                 R.drawable.ic_outline_bug,
                 "Report an issue",
                 "Having an issue? Report in here"
