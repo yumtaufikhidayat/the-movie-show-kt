6a4a23fc6312ca6742c8828161082dc54c4ef8da
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-03-19 06:42:45 +0700

    Update ApiModule.kt.

--- app/src/main/java/com/taufik/themovieshow/di/ApiModule.kt
+++ app/src/main/java/com/taufik/themovieshow/di/ApiModule.kt
@@ -1,5 +1,7 @@
 package com.taufik.themovieshow.di
 
+import android.content.Context
+import com.App
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.taufik.themovieshow.BuildConfig
@@ -9,16 +11,25 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
+
+    const val NETWORK_TIMEOUT = 60L
+    const val READ_TIMEOUT = 30
+    const val WRITE_TIMEOUT = 30
+    const val CONNECTION_TIMEOUT = 10
+    const val CACHE_SIZE_BYTES = 10 * 1024 * 1024L // 10 MB
 
     @Provides
     @Singleton
@@ -26,7 +37,7 @@ object ApiModule {
 
     @Provides
     @Singleton
-    fun connectionTimeout() = UrlEndpoint.NETWORK_TIMEOUT
+    fun connectionTimeout() = NETWORK_TIMEOUT
 
     @Provides
     @Singleton
@@ -34,7 +45,7 @@ object ApiModule {
 
     @Provides
     @Singleton
-    fun provideHttpClient() = if (BuildConfig.DEBUG) {
+    fun provideHttpClient(cache: Cache) = if (BuildConfig.DEBUG) {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS)
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
@@ -54,11 +65,28 @@ object ApiModule {
             return@Interceptor chain.proceed(request)
         }
 
-        OkHttpClient.Builder()
+        val okHttpClientBuilder = OkHttpClient.Builder()
+            .connectTimeout(CONNECTION_TIMEOUT.toLong(), TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT.toLong(), TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT.toLong(), TimeUnit.SECONDS)
+            .cache(cache)
             .addInterceptor(requestInterceptor)
             .addInterceptor(loggingInterceptor)
-            .build()
+        okHttpClientBuilder.build()
     } else OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    internal fun provideCache(context: Context): Cache {
+        val httpCacheDirectory = File(context.cacheDir.absolutePath, "HttpCache")
+        return Cache(httpCacheDirectory, CACHE_SIZE_BYTES)
+    }
+
+    @Provides
+    @Singleton
+    fun provideContext(application: App): Context {
+        return application.applicationContext
+    }
 
     @Singleton
     @Provides
