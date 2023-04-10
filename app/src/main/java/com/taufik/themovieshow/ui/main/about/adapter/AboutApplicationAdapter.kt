82305ac7a5f44526aa89651a25ddefd49f35b6e7
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-04-11 06:23:53 +0700

    Update adapter class using lambda and move clickListener to UI.

--- app/src/main/java/com/taufik/themovieshow/ui/main/about/adapter/AboutApplicationAdapter.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/about/adapter/AboutApplicationAdapter.kt
@@ -1,20 +1,16 @@
 package com.taufik.themovieshow.ui.main.about.adapter
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.taufik.themovieshow.databinding.ItemAboutBinding
 import com.taufik.themovieshow.model.response.about.About
-import es.dmoral.toasty.Toasty
 
-class AboutApplicationAdapter(val context: Context) :
-    ListAdapter<About, AboutApplicationAdapter.AboutViewHolder>(AboutAuthorDiffCallback) {
+class AboutApplicationAdapter(
+    private val onItemClick: (Int) -> Unit
+) : ListAdapter<About, AboutApplicationAdapter.AboutViewHolder>(aboutApplicationDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AboutViewHolder {
         return AboutViewHolder(
@@ -30,7 +26,8 @@ class AboutApplicationAdapter(val context: Context) :
         holder.bind(getItem(position))
     }
 
-    inner class AboutViewHolder(private val binding: ItemAboutBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class AboutViewHolder(private val binding: ItemAboutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(about: About) {
             binding.apply {
                 imgAbout.setImageResource(about.imgAbout)
@@ -38,81 +35,24 @@ class AboutApplicationAdapter(val context: Context) :
                 tvAboutDesc.text = about.descAbout
 
                 itemView.setOnClickListener {
-                    when (adapterPosition) {
-                        0 -> {
-                            val versionLink =
-                                "https://play.google.com/store/apps/details?id=com.taufik.themovieshow"
-                            try {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(versionLink))
-                                itemView.context.startActivity(
-                                    Intent.createChooser(
-                                        intent,
-                                        "Open with:"
-                                    )
-                                )
-                            } catch (e: Exception) {
-                                showToasty("Please install browser app")
-                            }
-                        }
-
-                        1 -> {
-                            val versionLink =
-                                "https://play.google.com/store/apps/details?id=com.taufik.themovieshow"
-                            try {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(versionLink))
-                                itemView.context.startActivity(
-                                    Intent.createChooser(
-                                        intent,
-                                        "Open with:"
-                                    )
-                                )
-                            } catch (e: Exception) {
-                                showToasty("Please install browser app")
-                            }
-                        }
-
-                        2 -> {
-                            val email = "yumtaufikhidayat@gmail.com"
-                            try {
-                                val intentEmail =
-                                    Intent(
-                                        Intent.ACTION_SENDTO,
-                                        Uri.fromParts("mailto", email, null)
-                                    )
-                                intentEmail.apply {
-                                    putExtra(Intent.EXTRA_EMAIL, email)
-                                    putExtra(Intent.EXTRA_SUBJECT, "")
-                                    putExtra(Intent.EXTRA_TEXT, "")
-                                }
-                                itemView.context.startActivity(
-                                    Intent.createChooser(
-                                        intentEmail,
-                                        "Send email"
-                                    )
-                                )
-                            } catch (e: java.lang.Exception) {
-                                showToasty("Please install email app")
-                            }
-                        }
-                    }
+                    onItemClick(layoutPosition)
                 }
             }
         }
     }
 
-    private fun showToasty(message: String) =
-        Toasty.error(context, message, Toast.LENGTH_SHORT).show()
+    companion object {
+        val aboutApplicationDiffCallback = object :
+            DiffUtil.ItemCallback<About>() {
+            override fun areItemsTheSame(
+                oldItem: About,
+                newItem: About
+            ): Boolean = oldItem.titleAbout == newItem.titleAbout
 
-    object AboutAuthorDiffCallback :
-        DiffUtil.ItemCallback<About>() {
-        override fun areItemsTheSame(
-            oldItem: About,
-            newItem: About
-        ): Boolean = oldItem.titleAbout == newItem.titleAbout
-
-        override fun areContentsTheSame(
-            oldItem: About,
-            newItem: About
-        ): Boolean = oldItem == newItem
+            override fun areContentsTheSame(
+                oldItem: About,
+                newItem: About
+            ): Boolean = oldItem == newItem
+        }
     }
 }
--- app/src/main/java/com/taufik/themovieshow/ui/main/about/adapter/AboutAuthorAdapter.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/about/adapter/AboutAuthorAdapter.kt
@@ -1,20 +1,16 @@
 package com.taufik.themovieshow.ui.main.about.adapter
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.taufik.themovieshow.databinding.ItemAboutBinding
 import com.taufik.themovieshow.model.response.about.About
-import es.dmoral.toasty.Toasty
 
-class AboutAuthorAdapter(val context: Context) :
-    ListAdapter<About, AboutAuthorAdapter.AboutViewHolder>(AboutAuthorDiffCallback) {
+class AboutAuthorAdapter(
+    private val onItemClick: (Int) -> Unit
+) : ListAdapter<About, AboutAuthorAdapter.AboutViewHolder>(aboutAuthorDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AboutViewHolder {
         return AboutViewHolder(
@@ -30,7 +26,8 @@ class AboutAuthorAdapter(val context: Context) :
         holder.bind(getItem(position))
     }
 
-    inner class AboutViewHolder(private val binding: ItemAboutBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class AboutViewHolder(private val binding: ItemAboutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(about: About) {
             binding.apply {
                 imgAbout.setImageResource(about.imgAbout)
@@ -38,79 +35,23 @@ class AboutAuthorAdapter(val context: Context) :
                 tvAboutDesc.text = about.descAbout
 
                 itemView.setOnClickListener {
-                    when (adapterPosition) {
-                        0 -> {
-                            val urlLink = "https://linkedin.com/in/taufik-hidayat"
-                            try {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlLink))
-                                itemView.context.startActivity(
-                                    Intent.createChooser(
-                                        intent,
-                                        "Open with:"
-                                    )
-                                )
-                            } catch (e: Exception) {
-                                showToasty("Please install browser app")
-                            }
-                        }
-
-                        1 -> {
-                            val githubLink = "https://github.com/yumtaufikhidayat/the-movie-show-kt"
-                            try {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(githubLink))
-                                itemView.context.startActivity(
-                                    Intent.createChooser(
-                                        intent,
-                                        "Open with:"
-                                    )
-                                )
-                            } catch (e: Exception) {
-                                showToasty("Please install browser app")
-                            }
-                        }
-
-                        2 -> {
-                            val email = "yumtaufikhidayat@gmail.com"
-                            try {
-                                val intentEmail =
-                                    Intent(
-                                        Intent.ACTION_SENDTO,
-                                        Uri.fromParts("mailto", email, null)
-                                    )
-                                intentEmail.apply {
-                                    putExtra(Intent.EXTRA_EMAIL, email)
-                                    putExtra(Intent.EXTRA_SUBJECT, "")
-                                    putExtra(Intent.EXTRA_TEXT, "")
-                                }
-                                itemView.context.startActivity(
-                                    Intent.createChooser(
-                                        intentEmail,
-                                        "Send email"
-                                    )
-                                )
-                            } catch (e: java.lang.Exception) {
-                                showToasty("Please install email app")
-                            }
-                        }
-                    }
+                    onItemClick(layoutPosition)
                 }
             }
         }
     }
 
-    private fun showToasty(message: String) =
-        Toasty.error(context, message, Toast.LENGTH_SHORT).show()
+    companion object {
+        val aboutAuthorDiffCallback = object : DiffUtil.ItemCallback<About>() {
+            override fun areItemsTheSame(
+                oldItem: About,
+                newItem: About
+            ): Boolean = oldItem.titleAbout == newItem.titleAbout
 
-    object AboutAuthorDiffCallback :
-        DiffUtil.ItemCallback<About>() {
-        override fun areItemsTheSame(
-            oldItem: About,
-            newItem: About
-        ): Boolean = oldItem.titleAbout == newItem.titleAbout
-
-        override fun areContentsTheSame(
-            oldItem: About,
-            newItem: About
-        ): Boolean = oldItem == newItem
+            override fun areContentsTheSame(
+                oldItem: About,
+                newItem: About
+            ): Boolean = oldItem == newItem
+        }
     }
 }
