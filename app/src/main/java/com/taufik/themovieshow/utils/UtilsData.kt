99bec2f1627b31def97348ed8bf9ac3ca1d1a3c8
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-04-10 09:24:32 +0700

    Move hardcoded string to string.xml file.

--- app/src/main/java/com/taufik/themovieshow/utils/UtilsData.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/UtilsData.kt
@@ -1,47 +1,52 @@
 package com.taufik.themovieshow.utils
 
+import android.content.Context
 import com.taufik.themovieshow.BuildConfig
 import com.taufik.themovieshow.R
 import com.taufik.themovieshow.model.response.about.About
 
 object UtilsData {
-    fun generateAboutAuthorData(): List<About> {
-
+    fun generateAboutAuthorData(context: Context): List<About> {
         return mutableListOf(
             About(
                 R.drawable.ic_outline_profile,
-                "Let's greet",
-                "Taufik Hidayat"
+                context.getString(R.string.tvTitleLetsGreet),
+                context.getString(R.string.tvDescLetsGreet)
             ),
             About(
                 R.drawable.github,
-                "Fork on Github",
-                "Fork this app on Github"
+                context.getString(R.string.tvTitleForkOnGithub),
+                context.getString(R.string.tvDescForkOnGithub),
             ),
             About(
                 R.drawable.ic_outline_email,
-                "Send an email",
-                "yumtaufikhidayat@gmail.com"
+                context.getString(R.string.tvTitleSendEmail),
+                context.getString(R.string.tvDescSendEmail),
             )
         )
     }
 
-    fun generateAboutApplicationData(): List<About> {
+    fun generateAboutApplicationData(context: Context): List<About> {
         return mutableListOf(
             About(
+                R.drawable.ic_outline_copyright,
+                context.getString(R.string.tvTitleCopyright),
+                context.getString(R.string.tvDescCopyright)
+            ),
+            About(
                 R.drawable.ic_update,
-                "Version",
+                context.getString(R.string.tvTitleVersion),
                 BuildConfig.VERSION_NAME
             ),
             About(
                 R.drawable.ic_outline_rate,
-                "Rate this app",
-                "Like this app? Rate 5 stars"
+                context.getString(R.string.tvTitleRateThisApp),
+                context.getString(R.string.tvDescRateThisApp)
             ),
             About(
                 R.drawable.ic_outline_bug,
-                "Report an issue",
-                "Having an issue? Report in here"
+                context.getString(R.string.tvTitleReportIssue),
+                context.getString(R.string.tvDescReportIssue)
             )
         )
     }
