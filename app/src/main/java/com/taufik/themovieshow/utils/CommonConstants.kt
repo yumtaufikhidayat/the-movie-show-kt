3f30ac55eabcfe272d1c5560696b47478cccc915
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-04-16 06:58:28 +0700

    Add constant variable to handle duplicate/hardcoded text.

--- app/src/main/java/com/taufik/themovieshow/utils/CommonConstants.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/CommonConstants.kt
@@ -2,4 +2,15 @@ package com.taufik.themovieshow.utils
 
 object CommonConstants {
     const val STARTING_PAGE_INDEX = 1
+    const val LOAD_PER_PAGE = 10
+    const val TABLE_NAME_FAVORITE_MOVIE_ENTITY = "favorite_movie"
+    const val COLUMN_NAME_POSTER = "poster"
+    const val COLUMN_NAME_TITLE = "title"
+    const val COLUMN_NAME_RELEASE_DATE = "release_date"
+    const val COLUMN_NAME_FIRST_AIR_DATE = "first_air_date"
+    const val COLUMN_NAME_RATING = "rating"
+    const val TABLE_NAME_FAVORITE_TV_SHOW_ENTITY = "favorite_tv_show"
+    const val DB_NAME = "the_movie_show_db"
+    const val DB_VERSION = 1
+    const val CURRENT_QUERY = "current_query"
 }
