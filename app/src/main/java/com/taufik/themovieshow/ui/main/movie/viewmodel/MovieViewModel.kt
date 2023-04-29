16fd5ee57ff4934e1e83751a2582b3235ba5e54d
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-04-29 09:22:16 +0700

    Change liveData to flow.

--- app/src/main/java/com/taufik/themovieshow/ui/main/movie/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/movie/viewmodel/MovieViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.taufik.themovieshow.data.NetworkResult
 import com.taufik.themovieshow.data.repository.TheMovieShowRepository
 import com.taufik.themovieshow.model.response.movie.discover.DiscoverMovieResponse
@@ -17,11 +18,11 @@ class MovieViewModel @Inject constructor(private val repository: TheMovieShowRep
     private val _discoverMovieResponse: MutableLiveData<NetworkResult<DiscoverMovieResponse>> = MutableLiveData()
     val discoverMovieResponse: LiveData<NetworkResult<DiscoverMovieResponse>> get() = _discoverMovieResponse
 
-    fun setMovieTrendingDay() = repository.getMovieTrendingDay()
+    fun setMovieTrendingDay() = repository.getMovieTrendingDay().cachedIn(viewModelScope)
 
-    fun setMovieNowPlaying() = repository.getMovieNowPlaying()
+    fun setMovieNowPlaying() = repository.getMovieNowPlaying().cachedIn(viewModelScope)
 
-    fun setMovieUpcoming() = repository.getMovieUpcoming()
+    fun setMovieUpcoming() = repository.getMovieUpcoming().cachedIn(viewModelScope)
 
     fun setDiscoverMovie(query: String) = viewModelScope.launch {
         repository.getDiscoverMovie(query).collect {
--- app/src/main/java/com/taufik/themovieshow/ui/main/tvshow/viewmodel/TvShowsViewModel.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/tvshow/viewmodel/TvShowsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.taufik.themovieshow.data.NetworkResult
 import com.taufik.themovieshow.data.repository.TheMovieShowRepository
 import com.taufik.themovieshow.model.response.tvshow.discover.DiscoverTvShowsResponse
@@ -17,11 +18,11 @@ class TvShowsViewModel @Inject constructor(private val repository: TheMovieShowR
     private val _discoverTvShowsResponse: MutableLiveData<NetworkResult<DiscoverTvShowsResponse>> = MutableLiveData()
     val discoverTvShowsResponse: LiveData<NetworkResult<DiscoverTvShowsResponse>> get() = _discoverTvShowsResponse
 
-    fun setTvShowsAiringToday() = repository.getTvShowsAiringToday()
+    fun setTvShowsAiringToday() = repository.getTvShowsAiringToday().cachedIn(viewModelScope)
 
-    fun setTvShowsPopular() = repository.getTvShowsPopular()
+    fun setTvShowsPopular() = repository.getTvShowsPopular().cachedIn(viewModelScope)
 
-    fun setTvShowsTrending() = repository.getTvShowsTrending()
+    fun setTvShowsTrending() = repository.getTvShowsTrending().cachedIn(viewModelScope)
 
     fun setDiscoverTvShows(query: String) = viewModelScope.launch {
         repository.getDiscoverTvShows(query).collect {
