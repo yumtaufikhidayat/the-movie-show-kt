5caa07ee051fd8b514fb8b2139b8c28426af6ff5
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-25 15:41:28 +0700

    Remove query page and add api key build config.

--- app/src/main/java/com/taufik/themovieshow/utils/CommonConstants.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/CommonConstants.kt
@@ -1,5 +1,7 @@
 package com.taufik.themovieshow.utils
 
+import com.taufik.themovieshow.BuildConfig
+
 object CommonConstants {
     const val STARTING_PAGE_INDEX = 1
     const val LOAD_PER_PAGE = 5
@@ -13,8 +15,9 @@ object CommonConstants {
     const val TABLE_NAME_FAVORITE_TV_SHOW_ENTITY = "favorite_tv_show"
     const val DB_NAME = "the_movie_show_db"
     const val DB_VERSION = 1
-    const val QUERY_PAGE = "page"
+    const val QUERY_API_KEY = "api_key"
     const val QUERY_Q = "query"
     const val QUERY_MOVIE_ID = "movie_id"
     const val QUERY_TV_SHOW_ID = "tv_id"
+    const val API_KEY = BuildConfig.API_KEY
 }
