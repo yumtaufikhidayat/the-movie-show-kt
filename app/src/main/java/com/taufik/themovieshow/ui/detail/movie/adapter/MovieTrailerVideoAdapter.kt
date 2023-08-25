14b71bceb2d438e2d93dbbf89491cb5767374231
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-25 15:53:16 +0700

    Add onItemClickListener into class constructor and update some its stuff.

--- app/src/main/java/com/taufik/themovieshow/ui/detail/movie/adapter/MovieTrailerVideoAdapter.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/detail/movie/adapter/MovieTrailerVideoAdapter.kt
@@ -1,7 +1,5 @@
 package com.taufik.themovieshow.ui.detail.movie.adapter
 
-import android.content.Intent
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -11,8 +9,11 @@ import com.taufik.themovieshow.databinding.ItemTrailerVideoBinding
 import com.taufik.themovieshow.model.response.movie.video.MovieVideoResult
 import com.taufik.themovieshow.utils.loadVideoThumbnail
 
-class MovieTrailerVideoAdapter :
-    ListAdapter<MovieVideoResult, MovieTrailerVideoAdapter.MovieTrailerViewHolder>(MovieTrailerDiffCallback) {
+class MovieTrailerVideoAdapter(
+    private val onItemClickListener: (MovieVideoResult) -> Unit
+) : ListAdapter<MovieVideoResult, MovieTrailerVideoAdapter.MovieTrailerViewHolder>(
+    MOVIE_TRAILER_DIFF_CALLBACK
+) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieTrailerViewHolder {
         return MovieTrailerViewHolder(
@@ -35,23 +36,24 @@ class MovieTrailerVideoAdapter :
                 imgPoster.loadVideoThumbnail(data.key)
                 tvTitle.text = data.name
                 cardTrailerVideo.setOnClickListener {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube://${data.key}"))
-                    itemView.context.startActivity(intent)
+                    onItemClickListener(data)
                 }
             }
         }
     }
 
-    object MovieTrailerDiffCallback :
-        DiffUtil.ItemCallback<MovieVideoResult>() {
-        override fun areItemsTheSame(
-            oldItem: MovieVideoResult,
-            newItem: MovieVideoResult
-        ): Boolean = oldItem == newItem
+    companion object {
+        val MOVIE_TRAILER_DIFF_CALLBACK = object :
+            DiffUtil.ItemCallback<MovieVideoResult>() {
+            override fun areItemsTheSame(
+                oldItem: MovieVideoResult,
+                newItem: MovieVideoResult
+            ): Boolean = oldItem == newItem
 
-        override fun areContentsTheSame(
-            oldItem: MovieVideoResult,
-            newItem: MovieVideoResult
-        ): Boolean = oldItem.id == newItem.id
+            override fun areContentsTheSame(
+                oldItem: MovieVideoResult,
+                newItem: MovieVideoResult
+            ): Boolean = oldItem.id == newItem.id
+        }
     }
 }
