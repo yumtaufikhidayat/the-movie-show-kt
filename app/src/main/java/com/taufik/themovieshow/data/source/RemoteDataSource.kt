8924eddb01a957020c618a2ba80d7f963acf9dc7
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-25 15:55:44 +0700

    Change some paging methods to common flow.

--- app/src/main/java/com/taufik/themovieshow/data/source/RemoteDataSource.kt
+++ app/src/main/java/com/taufik/themovieshow/data/source/RemoteDataSource.kt
@@ -1,26 +1,22 @@
 package com.taufik.themovieshow.data.source
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import com.taufik.themovieshow.data.BaseApiResponse
 import com.taufik.themovieshow.data.NetworkResult
-import com.taufik.themovieshow.data.paging.movie.MovieNowPlayingPagingSource
-import com.taufik.themovieshow.data.paging.movie.MovieTrendingPagingSource
-import com.taufik.themovieshow.data.paging.movie.MovieUpcomingPagingSource
-import com.taufik.themovieshow.data.paging.tvshow.TvShowsAiringTodayPagingSource
-import com.taufik.themovieshow.data.paging.tvshow.TvShowsPopularPagingSource
-import com.taufik.themovieshow.data.paging.tvshow.TvShowsTrendingPagingSource
 import com.taufik.themovieshow.data.remote.api.ApiService
 import com.taufik.themovieshow.model.response.common.reviews.ReviewsResponse
 import com.taufik.themovieshow.model.response.movie.cast.MovieCastResponse
 import com.taufik.themovieshow.model.response.movie.detail.MovieDetailResponse
 import com.taufik.themovieshow.model.response.movie.discover.DiscoverMovieResponse
+import com.taufik.themovieshow.model.response.movie.nowplayingupcoming.MovieMainResponse
 import com.taufik.themovieshow.model.response.movie.similar.MovieSimilarResponse
+import com.taufik.themovieshow.model.response.movie.trending.MovieTrendingResponse
 import com.taufik.themovieshow.model.response.movie.video.MovieVideoResponse
 import com.taufik.themovieshow.model.response.tvshow.cast.TvShowsCastResponse
 import com.taufik.themovieshow.model.response.tvshow.detail.TvShowsPopularDetailResponse
 import com.taufik.themovieshow.model.response.tvshow.discover.DiscoverTvShowsResponse
+import com.taufik.themovieshow.model.response.tvshow.popularairingtoday.TvShowsMainResponse
 import com.taufik.themovieshow.model.response.tvshow.similar.TvShowsSimilarResponse
+import com.taufik.themovieshow.model.response.tvshow.trending.TvShowsTrendingResponse
 import com.taufik.themovieshow.model.response.tvshow.video.TvShowsVideoResponse
 import com.taufik.themovieshow.utils.CommonConstants
 import kotlinx.coroutines.Dispatchers
@@ -30,124 +26,114 @@ import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(private val apiService: ApiService): BaseApiResponse() {
+
     private val dispatchersIO = Dispatchers.IO
+    private val apiKey = CommonConstants.API_KEY
 
-    fun getMovieNowPlaying() = Pager(
-        PagingConfig(
-            pageSize = CommonConstants.LOAD_PER_PAGE,
-            enablePlaceholders = false
-        ), pagingSourceFactory = {
-            MovieNowPlayingPagingSource(apiService)
-        }).flow
+    fun getMovieTrendingDay(): Flow<NetworkResult<MovieTrendingResponse>> {
+        return flow {
+            emit(safeApiCall { apiService.getMovieTrendingDay(apiKey) })
+        }.flowOn(dispatchersIO)
+    }
 
-    fun getMovieUpcoming() = Pager(
-        PagingConfig(
-            pageSize = CommonConstants.LOAD_PER_PAGE,
-            enablePlaceholders = false
-        ), pagingSourceFactory = {
-            MovieUpcomingPagingSource(apiService)
-        }).flow
+    fun getMovieNowPlaying(): Flow<NetworkResult<MovieMainResponse>> {
+        return flow {
+            emit(safeApiCall { apiService.getMovieNowPlaying(apiKey) })
+        }.flowOn(dispatchersIO)
+    }
 
-    fun getMovieTrendingDay() = Pager(
-        PagingConfig(
-            pageSize = CommonConstants.LOAD_MAX_PER_PAGE,
-            enablePlaceholders = false
-        ), pagingSourceFactory = {
-            MovieTrendingPagingSource(apiService)
-        }).flow
+    fun getMovieUpcoming(): Flow<NetworkResult<MovieMainResponse>> {
+        return flow {
+            emit(safeApiCall { apiService.getMovieUpcoming(apiKey) })
+        }.flowOn(dispatchersIO)
+    }
 
     suspend fun getDiscoverMovie(query: String): Flow<NetworkResult<DiscoverMovieResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getDiscoverMovie(query) })
+            emit(safeApiCall { apiService.getDiscoverMovie(apiKey, query) })
         }.flowOn(dispatchersIO)
     }
     suspend fun getMovieVideo(movieId: Int) : Flow<NetworkResult<MovieVideoResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getMovieVideo(movieId) })
+            emit(safeApiCall { apiService.getMovieVideo(apiKey, movieId) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getMovieCast(movieId: Int): Flow<NetworkResult<MovieCastResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getMovieCast(movieId) })
+            emit(safeApiCall { apiService.getMovieCast(movieId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getDetailMovie(movieId: Int) : Flow<NetworkResult<MovieDetailResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getDetailMovie(movieId) })
+            emit(safeApiCall { apiService.getDetailMovie(movieId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getMovieReviews(movieId: Int) : Flow<NetworkResult<ReviewsResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getReviewsMovie(movieId) })
+            emit(safeApiCall { apiService.getReviewsMovie(movieId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getSimilarMovie(movieId: Int) : Flow<NetworkResult<MovieSimilarResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getSimilarMovie(movieId) })
+            emit(safeApiCall { apiService.getSimilarMovie(movieId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
-    fun getTvShowsAiringToday() = Pager(
-        PagingConfig(
-            pageSize = CommonConstants.LOAD_PER_PAGE,
-            enablePlaceholders = false
-        ), pagingSourceFactory = {
-            TvShowsAiringTodayPagingSource(apiService)
-        }).flow
+    fun getTvShowsTrending(): Flow<NetworkResult<TvShowsTrendingResponse>> {
+        return flow {
+            emit(safeApiCall { apiService.getTvShowsTrending(apiKey) })
+        }.flowOn(dispatchersIO)
+    }
 
-    fun getTvShowsPopular() = Pager(
-        PagingConfig(
-            pageSize = CommonConstants.LOAD_PER_PAGE,
-            enablePlaceholders = false
-        ), pagingSourceFactory = {
-            TvShowsPopularPagingSource(apiService)
-        }).flow
+    fun getTvShowsAiringToday(): Flow<NetworkResult<TvShowsMainResponse>> {
+        return flow {
+            emit(safeApiCall { apiService.getTvShowsAiringToday(apiKey) })
+        }.flowOn(dispatchersIO)
+    }
 
-    fun getTvShowsTrending() = Pager(
-        PagingConfig(
-            pageSize = CommonConstants.LOAD_PER_PAGE,
-            enablePlaceholders = false
-        ), pagingSourceFactory = {
-            TvShowsTrendingPagingSource(apiService)
-        }).flow
+    fun getTvShowsPopular(): Flow<NetworkResult<TvShowsMainResponse>> {
+        return flow {
+            emit(safeApiCall { apiService.getTvShowsPopular(apiKey) })
+        }.flowOn(dispatchersIO)
+    }
 
     suspend fun getDiscoverTvShows(query: String): Flow<NetworkResult<DiscoverTvShowsResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getDiscoverTvShows(query) })
+            emit(safeApiCall { apiService.getDiscoverTvShows(apiKey, query) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getTvShowsVideo(tvId: Int) : Flow<NetworkResult<TvShowsVideoResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getTvShowsVideo(tvId) })
+            emit(safeApiCall { apiService.getTvShowsVideo(tvId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getTvShowsCast(tvId: Int) : Flow<NetworkResult<TvShowsCastResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getTvShowsCast(tvId) })
+            emit(safeApiCall { apiService.getTvShowsCast(tvId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getDetailTvShows(tvId: Int) : Flow<NetworkResult<TvShowsPopularDetailResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getDetailTvShows(tvId) })
+            emit(safeApiCall { apiService.getDetailTvShows(tvId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getTvShowsReviews(tvId: Int): Flow<NetworkResult<ReviewsResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getReviewsTvShows(tvId) })
+            emit(safeApiCall { apiService.getReviewsTvShows(tvId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 
     suspend fun getSimilarTvShows(tvId: Int): Flow<NetworkResult<TvShowsSimilarResponse>> {
         return flow {
-            emit(safeApiCall { apiService.getSimilarTvShows(tvId) })
+            emit(safeApiCall { apiService.getSimilarTvShows(tvId, apiKey) })
         }.flowOn(dispatchersIO)
     }
 }
