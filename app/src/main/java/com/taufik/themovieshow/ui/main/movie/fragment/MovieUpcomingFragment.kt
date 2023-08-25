c0efa4543ec7eafd4a3c265776352493a310ec72
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-25 15:54:08 +0700

    Change paging to common recyclerview.

--- app/src/main/java/com/taufik/themovieshow/ui/main/movie/fragment/MovieUpcomingFragment.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/movie/fragment/MovieUpcomingFragment.kt
@@ -4,48 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.taufik.themovieshow.R
+import com.taufik.themovieshow.data.NetworkResult
 import com.taufik.themovieshow.databinding.FragmentMovieTvShowsListBinding
-import com.taufik.themovieshow.ui.detail.movie.fragment.DetailMovieFragment
-import com.taufik.themovieshow.ui.main.LoadMoreAdapter
 import com.taufik.themovieshow.ui.main.movie.adapter.MovieAdapter
 import com.taufik.themovieshow.ui.main.movie.viewmodel.MovieViewModel
+import com.taufik.themovieshow.utils.navigateToDetailMovie
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MovieUpcomingFragment : Fragment() {
 
-    private var _binding: FragmentMovieTvShowsListBinding? = null
+    private var _binding : FragmentMovieTvShowsListBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<MovieViewModel>()
     private var movieAdapter: MovieAdapter? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentMovieTvShowsListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         setAdapter()
         setData()
     }
 
     private fun setAdapter() {
+        movieAdapter = MovieAdapter {
+            navigateToDetailMovie(it.id, it.title)
+        }
         binding.rvCommon.apply {
             layoutManager = LinearLayoutManager(requireContext())
             setHasFixedSize(true)
@@ -55,71 +53,34 @@ class MovieUpcomingFragment : Fragment() {
 
     private fun setData() {
         binding.apply {
-            movieAdapter = MovieAdapter {
-                val bundle = Bundle().apply {
-                    putInt(DetailMovieFragment.EXTRA_ID, it.id)
-                    putString(DetailMovieFragment.EXTRA_TITLE, it.title)
-                }
-                findNavController().navigate(R.id.detailMovieFragment, bundle)
-            }
-
-            lifecycleScope.launch {
-                viewModel.setMovieUpcoming().collect {
-                    movieAdapter?.submitData(viewLifecycleOwner.lifecycle, it)
-                }
-            }
-
-            movieAdapter?.apply {
-                layoutError.apply {
-                    addLoadStateListener { loadState ->
-                        val loadStateRefresh = loadState.source.refresh
-                        pbLoading.isVisible = loadStateRefresh is LoadState.Loading
-                        rvCommon.isVisible = loadStateRefresh is LoadState.NotLoading
-                        tvErrorTitle.apply {
-                            isVisible = loadStateRefresh is LoadState.Error
-                            text = getString(R.string.tvOops)
-                            setTextColor(
-                                ContextCompat.getColor(
-                                    requireContext(),
-                                    R.color.colorOrange
-                                )
-                            )
-                        }
-                        tvError.apply {
-                            isVisible = loadStateRefresh is LoadState.Error
-                            text = getString(R.string.tvUnableLoadData)
-                            setTextColor(
-                                ContextCompat.getColor(
-                                    requireContext(),
-                                    R.color.colorOrange
-                                )
-                            )
-                        }
-                        btnRetry.isVisible = loadStateRefresh is LoadState.Error
-
-                        if (loadStateRefresh is LoadState.NotLoading
-                            && loadState.append.endOfPaginationReached
-                            && itemCount < 1
-                        ) {
-                            rvCommon.isVisible = false
-                            tvEmptyTitle.isVisible = true
-                            tvEmpty.isVisible = true
-                        } else {
-                            tvEmptyTitle.isVisible = false
-                            tvEmpty.isVisible = false
-                        }
+            viewModel.getMovieUpcoming().observe(viewLifecycleOwner) {
+                when (it) {
+                    is NetworkResult.Loading -> showLoading(true)
+                    is NetworkResult.Success -> {
+                        showLoading(false)
+                        movieAdapter?.submitList(it.data?.results)
                     }
-
-                    btnRetry.setOnClickListener {
-                        movieAdapter?.retry()
+                    is NetworkResult.Error -> {
+                        showLoading(false)
+                        showError(it.message)
+                    }
+                    else -> {
+                        showLoading(false)
+                        showError(it.message)
                     }
                 }
             }
+        }
+    }
 
-            rvCommon.adapter = movieAdapter?.withLoadStateHeaderAndFooter(
-                header = LoadMoreAdapter { movieAdapter?.retry() },
-                footer = LoadMoreAdapter { movieAdapter?.retry() }
-            )
+    private fun showLoading(isShow: Boolean) {
+        binding.pbLoading.visibility = if (isShow) View.VISIBLE else View.GONE
+    }
+
+    private fun showError(message: String?) {
+        binding.layoutError.apply {
+            root.isVisible = true
+            tvErrorDesc.text = message
         }
     }
 
