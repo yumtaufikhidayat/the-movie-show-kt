264049a8d80cdf4a2d618b221f6a9aaa04a69efc
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-25 15:54:46 +0700

    Change some methods as livedata.

--- app/src/main/java/com/taufik/themovieshow/ui/main/movie/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/main/movie/viewmodel/MovieViewModel.kt
@@ -3,8 +3,8 @@ package com.taufik.themovieshow.ui.main.movie.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import androidx.paging.cachedIn
 import com.taufik.themovieshow.data.NetworkResult
 import com.taufik.themovieshow.data.repository.TheMovieShowRepository
 import com.taufik.themovieshow.model.response.movie.discover.DiscoverMovieResponse
@@ -18,11 +18,11 @@ class MovieViewModel @Inject constructor(private val repository: TheMovieShowRep
     private val _discoverMovieResponse: MutableLiveData<NetworkResult<DiscoverMovieResponse>> = MutableLiveData()
     val discoverMovieResponse: LiveData<NetworkResult<DiscoverMovieResponse>> get() = _discoverMovieResponse
 
-    fun setMovieTrendingDay() = repository.getMovieTrendingDay().cachedIn(viewModelScope)
+    fun getMovieTrendingDay() = repository.getMovieTrendingDay().asLiveData()
 
-    fun setMovieNowPlaying() = repository.getMovieNowPlaying().cachedIn(viewModelScope)
+    fun getMovieNowPlaying() = repository.getMovieNowPlaying().asLiveData()
 
-    fun setMovieUpcoming() = repository.getMovieUpcoming().cachedIn(viewModelScope)
+    fun getMovieUpcoming() = repository.getMovieUpcoming().asLiveData()
 
     fun setDiscoverMovie(query: String) = viewModelScope.launch {
         repository.getDiscoverMovie(query).collect {
