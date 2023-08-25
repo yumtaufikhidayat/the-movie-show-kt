657e8b3726286fed3119d547f17d9063da579bbe
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-25 15:37:21 +0700

    Change the position of trending movie and trending tv show function. Also remove page query and add api key query.

--- app/src/main/java/com/taufik/themovieshow/data/remote/api/ApiService.kt
+++ app/src/main/java/com/taufik/themovieshow/data/remote/api/ApiService.kt
@@ -23,93 +23,105 @@ import retrofit2.http.Query
 
 interface ApiService {
 
+    @GET(UrlEndpoint.MOVIE_TRENDING_DAY)
+    suspend fun getMovieTrendingDay(
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
+    ): Response<MovieTrendingResponse>
+
     @GET(UrlEndpoint.MOVIE_NOW_PLAYING)
     suspend fun getMovieNowPlaying(
-        @Query(CommonConstants.QUERY_PAGE) page: Int
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
     ): Response<MovieMainResponse>
 
     @GET(UrlEndpoint.MOVIE_UPCOMING)
     suspend fun getMovieUpcoming(
-        @Query(CommonConstants.QUERY_PAGE) page: Int
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
     ): Response<MovieMainResponse>
-
-    @GET(UrlEndpoint.MOVIE_TRENDING_DAY)
-    suspend fun getMovieTrendingDay(
-        @Query(CommonConstants.QUERY_PAGE) page: Int
-    ): Response<MovieTrendingResponse>
 
     @GET(UrlEndpoint.DISCOVER_MOVIES)
     suspend fun getDiscoverMovie(
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
         @Query(CommonConstants.QUERY_Q) query: String
     ): Response<DiscoverMovieResponse>
 
     @GET(UrlEndpoint.MOVIE_VIDEO)
     suspend fun getMovieVideo(
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
         @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int
     ): Response<MovieVideoResponse>
 
     @GET(UrlEndpoint.MOVIE_CAST)
     suspend fun getMovieCast(
-        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int
+        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<MovieCastResponse>
 
     @GET(UrlEndpoint.MOVIE_DETAIL)
     suspend fun getDetailMovie(
-        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int
+        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<MovieDetailResponse>
 
     @GET(UrlEndpoint.MOVIE_REVIEWS)
     suspend fun getReviewsMovie(
-        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int
+        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<ReviewsResponse>
 
     @GET(UrlEndpoint.MOVIE_SIMILAR)
     suspend fun getSimilarMovie(
-        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int
+        @Path(CommonConstants.QUERY_MOVIE_ID) movieId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<MovieSimilarResponse>
+
+    @GET(UrlEndpoint.TV_SHOWS_TRENDING_DAY)
+    suspend fun getTvShowsTrending(
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
+    ): Response<TvShowsTrendingResponse>
 
     @GET(UrlEndpoint.TV_SHOWS_AIRING_TODAY)
     suspend fun getTvShowsAiringToday(
-        @Query(CommonConstants.QUERY_PAGE) page: Int
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
     ): Response<TvShowsMainResponse>
 
     @GET(UrlEndpoint.TV_SHOWS_POPULAR)
     suspend fun getTvShowsPopular(
-        @Query(CommonConstants.QUERY_PAGE) page: Int
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
     ): Response<TvShowsMainResponse>
-
-    @GET(UrlEndpoint.TV_SHOWS_TRENDING_DAY)
-    suspend fun getTvShowsTrending(
-        @Query(CommonConstants.QUERY_PAGE) page: Int
-    ): Response<TvShowsTrendingResponse>
 
     @GET(UrlEndpoint.DISCOVER_TV_SHOWS)
     suspend fun getDiscoverTvShows(
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String,
         @Query(CommonConstants.QUERY_Q) query: String
     ): Response<DiscoverTvShowsResponse>
 
     @GET(UrlEndpoint.TV_SHOWS_VIDEO)
     suspend fun getTvShowsVideo(
-        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int
+        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<TvShowsVideoResponse>
 
     @GET(UrlEndpoint.TV_SHOWS_CAST)
     suspend fun getTvShowsCast(
-        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int
+        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<TvShowsCastResponse>
 
     @GET(UrlEndpoint.TV_SHOWS_DETAIL)
     suspend fun getDetailTvShows(
-        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int
+        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<TvShowsPopularDetailResponse>
 
     @GET(UrlEndpoint.TV_SHOWS_REVIEWS)
     suspend fun getReviewsTvShows(
-        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int
+        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<ReviewsResponse>
 
     @GET(UrlEndpoint.TV_SHOWS_SIMILAR)
     suspend fun getSimilarTvShows(
-        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int
+        @Path(CommonConstants.QUERY_TV_SHOW_ID) tvId: Int,
+        @Query(CommonConstants.QUERY_API_KEY) apiKey: String
     ): Response<TvShowsSimilarResponse>
 }
