7e2d9ed6dac1bf020d9601efe4852732a85bee2f
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-10-09 10:42:40 +0700

    Rename class and make it reusable/common class.

--- app/src/main/java/com/taufik/themovieshow/ui/common/adapter/TabPagerAdapter.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/common/adapter/TabPagerAdapter.kt
@@ -1,13 +1,12 @@
-package com.taufik.themovieshow.ui.movie.adapter
+package com.taufik.themovieshow.ui.common.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.taufik.themovieshow.ui.movie.fragment.MovieNowPlayingFragment
-import com.taufik.themovieshow.ui.movie.fragment.MovieUpcomingFragment
 
-class MoviePagerAdapter(
+class TabPagerAdapter(
     private val listOfFragments: List<Fragment>,
-    fragment: Fragment) : FragmentStateAdapter(fragment) {
+    fragment: Fragment
+) : FragmentStateAdapter(fragment) {
 
     override fun createFragment(position: Int): Fragment = listOfFragments[position]
 
