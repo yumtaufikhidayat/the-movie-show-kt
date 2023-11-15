c19f32a212b13dacb31a5f2b2f7972b8324b9ea8
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-11-15 09:50:39 +0700

    Remove genre model classes.

--- app/src/main/java/com/taufik/themovieshow/model/response/movie/genre/GenresResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.taufik.themovieshow.model.response.movie.genre
-
-
-import com.google.gson.annotations.SerializedName
-
-data class GenresResponse(
-    @SerializedName("genres")
-    val genres: List<Genre> = listOf()
-)
