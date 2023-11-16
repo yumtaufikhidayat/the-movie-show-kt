04e19b7c15522aefde95b8bd2b51eab6b21c9dff
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-11-16 10:08:25 +0700

    Rename file to Kotlin DSL.

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+}
