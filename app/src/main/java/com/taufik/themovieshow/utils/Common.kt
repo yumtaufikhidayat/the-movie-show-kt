2ec0a9687c276b8f0702e5308f1d2646c92af2b5
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-11-24 18:39:15 +0700

    Add FavoriteTvShowsFragment position key.

--- app/src/main/java/com/taufik/themovieshow/utils/Common.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/Common.kt
@@ -15,6 +15,7 @@ import com.taufik.themovieshow.data.remote.api.UrlEndpoint
 import com.taufik.themovieshow.ui.detail.movie.fragment.DetailMovieFragment
 import com.taufik.themovieshow.ui.detail.tvshow.fragment.DetailTvShowFragment
 import com.taufik.themovieshow.ui.favorite.fragment.FavoriteMovieFragment
+import com.taufik.themovieshow.ui.favorite.fragment.FavoriteTvShowsFragment
 import es.dmoral.toasty.Toasty
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -67,10 +68,11 @@ fun Fragment.navigateToDetailMovie(id: Int, title: String, position: Int) {
     this.findNavController().navigate(R.id.detailMovieFragment, bundle)
 }
 
-fun Fragment.navigateToDetailTvShow(id: Int, title: String) {
+fun Fragment.navigateToDetailTvShow(id: Int, title: String, position: Int) {
     val bundle = Bundle().apply {
         putInt(DetailTvShowFragment.EXTRA_ID, id)
         putString(DetailTvShowFragment.EXTRA_TITLE, title)
+        putInt(FavoriteTvShowsFragment.POSITION_KEY, position)
     }
     this.findNavController().navigate(R.id.detailTvShowFragment, bundle)
 }
