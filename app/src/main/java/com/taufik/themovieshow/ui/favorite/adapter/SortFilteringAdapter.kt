190dc03ceea58443ecf3562146abb33c8b6dbe88
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-12-05 11:36:34 +0700

    Remove invoke by nameRes to selectedItemPosition.

--- app/src/main/java/com/taufik/themovieshow/ui/favorite/adapter/SortFilteringAdapter.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/favorite/adapter/SortFilteringAdapter.kt
@@ -54,8 +54,7 @@ class SortFilteringAdapter(
                         notifyItemChanged(previousSelected)
                         notifyItemChanged(selectedItemPosition)
 
-                        onItemClickListener?.invoke(data.sortNameRes)
-//                        onItemClickListener?.invoke(selectedItemPosition)
+                        onItemClickListener?.invoke(selectedItemPosition)
                     }
                 }
             }
