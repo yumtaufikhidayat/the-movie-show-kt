b5a0673435e61b9afd7d9e7ccccacad1b69836bb
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-06-07 16:47:05 +0700

    Feat: Refactor to Flow - Refactor LiveData/MutableLiveData to Flow in DetailMovieTvShowViewModel.kt, FavoriteMovieViewModel.kt, and FavoriteTvShowViewModel.kt - Remove LiveDataExt.kt

--- app/src/main/java/com/taufik/themovieshow/ui/detail/movie_tvshow/viewmodel/DetailMovieTvShowViewModel.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/detail/movie_tvshow/viewmodel/DetailMovieTvShowViewModel.kt
@@ -1,7 +1,5 @@
 package com.taufik.themovieshow.ui.detail.movie_tvshow.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.taufik.themovieshow.data.NetworkResult
@@ -18,6 +16,9 @@ import com.taufik.themovieshow.model.response.tvshow.detail.TvShowsPopularDetail
 import com.taufik.themovieshow.model.response.tvshow.similar.TvShowsSimilarResponse
 import com.taufik.themovieshow.model.response.tvshow.video.TvShowsVideoResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,67 +28,44 @@ class DetailMovieTvShowViewModel @Inject constructor(private val repository: The
     var idMovieTvShow = 0
     var titleMovieTvShow = ""
 
-    private val _detailMoviesResponse: MutableLiveData<NetworkResult<MovieDetailResponse>> = MutableLiveData()
-    val detailMoviesResponse: LiveData<NetworkResult<MovieDetailResponse>> = _detailMoviesResponse
+    // Movies
+    fun detailMoviesResponse(movieId: Int): StateFlow<NetworkResult<MovieDetailResponse>> =
+        repository.getDetailMovie(movieId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    private val _detailMoviesCastResponse: MutableLiveData<NetworkResult<MovieCastResponse>> = MutableLiveData()
-    val detailMoviesCastResponse: LiveData<NetworkResult<MovieCastResponse>> = _detailMoviesCastResponse
+    fun detailMoviesCastResponse(movieId: Int): StateFlow<NetworkResult<MovieCastResponse>> =
+        repository.getMovieCast(movieId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    private val _detailMoviesVideoResponse: MutableLiveData<NetworkResult<MovieVideoResponse>> = MutableLiveData()
-    val detailMoviesVideoResponse: LiveData<NetworkResult<MovieVideoResponse>> = _detailMoviesVideoResponse
+    fun detailMoviesVideoResponse(movieId: Int): StateFlow<NetworkResult<MovieVideoResponse>> =
+        repository.getMovieVideo(movieId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    private val _detailMovieReviewsResponse: MutableLiveData<NetworkResult<ReviewsResponse>> = MutableLiveData()
-    val detailMovieReviewsResponse: LiveData<NetworkResult<ReviewsResponse>> = _detailMovieReviewsResponse
 
-    private val _detailMovieSimilarResponse: MutableLiveData<NetworkResult<MovieSimilarResponse>> = MutableLiveData()
-    val detailMovieSimilarResponse: LiveData<NetworkResult<MovieSimilarResponse>> = _detailMovieSimilarResponse
+    fun detailMovieReviewsResponse(movieId: Int): StateFlow<NetworkResult<ReviewsResponse>> =
+        repository.getMovieReviews(movieId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    private val _detailTvShowResponse: MutableLiveData<NetworkResult<TvShowsPopularDetailResponse>> = MutableLiveData()
-    val detailTvShowResponse: LiveData<NetworkResult<TvShowsPopularDetailResponse>> = _detailTvShowResponse
+    fun detailMovieSimilarResponse(movieId: Int): StateFlow<NetworkResult<MovieSimilarResponse>> =
+        repository.getSimilarMovie(movieId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    private val _detailTvShowVideoResponse: MutableLiveData<NetworkResult<TvShowsVideoResponse>> = MutableLiveData()
-    val detailTvShowVideoResponse: LiveData<NetworkResult<TvShowsVideoResponse>> = _detailTvShowVideoResponse
-
-    private val _detailTvShowCastResponse: MutableLiveData<NetworkResult<TvShowsCastResponse>> = MutableLiveData()
-    val detailTvShowCastResponse: LiveData<NetworkResult<TvShowsCastResponse>> = _detailTvShowCastResponse
-
-    private val _detailTvShowReviewsResponse: MutableLiveData<NetworkResult<ReviewsResponse>> = MutableLiveData()
-    val detailTvShowReviewsResponse: LiveData<NetworkResult<ReviewsResponse>> = _detailTvShowReviewsResponse
-
-    private val _detailTvShowSimilarResponse: MutableLiveData<NetworkResult<TvShowsSimilarResponse>> = MutableLiveData()
-    val detailTvShowSimilarResponse: LiveData<NetworkResult<TvShowsSimilarResponse>> = _detailTvShowSimilarResponse
-
-    fun setDetailMovies(id: Int) = viewModelScope.launch {
-        repository.getDetailMovie(id).collect {
-            _detailMoviesResponse.value = it
-        }
-    }
-
-    fun setDetailMovieCast(id: Int) = viewModelScope.launch {
-        repository.getMovieCast(id).collect {
-            _detailMoviesCastResponse.value = it
-        }
-    }
-
-    fun setDetailMovieVideo(id: Int) = viewModelScope.launch {
-        repository.getMovieVideo(id).collect {
-            _detailMoviesVideoResponse.value = it
-        }
-    }
-
-    fun setDetailMovieReviews(id: Int) = viewModelScope.launch {
-        repository.getMovieReviews(id).collect {
-            _detailMovieReviewsResponse.value = it
-        }
-    }
-
-    fun setDetailMovieSimilar(id: Int) = viewModelScope.launch {
-        repository.getSimilarMovie(id).collect {
-            _detailMovieSimilarResponse.value = it
-        }
-    }
-
-    // Favorite
+    // Favorite Movie
     fun addMovieToFavorite(
         movieId: Int,
         posterPath: String,
@@ -96,11 +74,7 @@ class DetailMovieTvShowViewModel @Inject constructor(private val repository: The
         rating: Double
     ) {
         viewModelScope.launch {
-            repository.addMovieToFavorite(
-                FavoriteMovieEntity(
-                    movieId, posterPath, title, releaseDate, rating
-                )
-            )
+            repository.addMovieToFavorite(FavoriteMovieEntity(movieId, posterPath, title, releaseDate, rating))
         }
     }
 
@@ -112,35 +86,41 @@ class DetailMovieTvShowViewModel @Inject constructor(private val repository: The
         }
     }
 
-    fun setDetailTvShow(id: Int) = viewModelScope.launch {
-        repository.getDetailTvShows(id).collect {
-            _detailTvShowResponse.value = it
-        }
-    }
+    // TV Shows
+    fun detailTvShowResponse(tvShowId: Int): StateFlow<NetworkResult<TvShowsPopularDetailResponse>> =
+        repository.getDetailTvShows(tvShowId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    fun setDetailTvShowVideo(id: Int) = viewModelScope.launch {
-        repository.getTvShowsVideo(id).collect {
-            _detailTvShowVideoResponse.value = it
-        }
-    }
+    fun detailTvShowCastResponse(tvShowId: Int): StateFlow<NetworkResult<TvShowsCastResponse>> =
+        repository.getTvShowsCast(tvShowId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    fun setDetailTvShowsCast(id: Int) = viewModelScope.launch {
-        repository.getTvShowsCast(id).collect {
-            _detailTvShowCastResponse.value = it
-        }
-    }
+    fun detailTvShowVideoResponse(tvShowId: Int): StateFlow<NetworkResult<TvShowsVideoResponse>> =
+        repository.getTvShowsVideo(tvShowId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    fun setDetailTvShowsReviews(id: Int) = viewModelScope.launch {
-        repository.getTvShowsReviews(id).collect {
-            _detailTvShowReviewsResponse.value = it
-        }
-    }
+    fun detailTvShowReviewsResponse(tvShowId: Int): StateFlow<NetworkResult<ReviewsResponse>> =
+        repository.getTvShowsReviews(tvShowId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
-    fun setDetailTvShowsSimilar(id: Int) = viewModelScope.launch {
-        repository.getSimilarTvShows(id).collect {
-            _detailTvShowSimilarResponse.value = it
-        }
-    }
+    fun detailTvShowSimilarResponse(tvShowId: Int): StateFlow<NetworkResult<TvShowsSimilarResponse>> =
+        repository.getSimilarTvShows(tvShowId).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(DELAY_EMIT),
+            initialValue = NetworkResult.Loading()
+        )
 
     // Favorite
     fun addTvShowFavorite(
@@ -169,5 +149,6 @@ class DetailMovieTvShowViewModel @Inject constructor(private val repository: The
 
     companion object {
         const val DELAY_SCROLL_TO_TOP_POSITION = 100L
+        const val DELAY_EMIT = 5000L
     }
 }
