b454163a6237d00313aca908e1ef6c5ee4eb88d6
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-06-27 19:18:39 +0700

    Refactor: Remove unused LANGUAGE_CHANGED constant The `LANGUAGE_CHANGED` constant in `LanguageBottomSheetDialog.kt` was not being used and has been removed.

--- app/src/main/java/com/taufik/themovieshow/ui/language/bottomsheet/LanguageBottomSheetDialog.kt
+++ app/src/main/java/com/taufik/themovieshow/ui/language/bottomsheet/LanguageBottomSheetDialog.kt
@@ -84,7 +84,6 @@ class LanguageBottomSheetDialog : BottomSheetDialogFragment() {
     }
 
     companion object {
-        const val LANGUAGE_CHANGED = "language_changed"
         const val SUCCESS_CHANGE_LANGUAGE = "success_change_language"
         private const val ARG_LANGUAGE_LIST = "arg_language_list"
         private const val ARG_SELECTED_LANG_CODE = "arg_selected_lang"
