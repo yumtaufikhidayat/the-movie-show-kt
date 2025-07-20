eb8f8d89d31931d5ce75eb7c6985d0834123923a
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-07-20 21:01:46 +0700

    Feat: Add `toThousandFormat` extension function for Int.

--- app/src/main/java/com/taufik/themovieshow/utils/extensions/IntExt.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/extensions/IntExt.kt
@@ -2,9 +2,19 @@ package com.taufik.themovieshow.utils.extensions
 
 import android.content.res.Resources
 import android.util.DisplayMetrics
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import kotlin.math.roundToInt
 
 fun convertDpToPx(dp: Int): Int {
     val metrics = Resources.getSystem().displayMetrics
     return (dp * (metrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+}
+
+fun Int.toThousandFormat(): String {
+    val symbols = DecimalFormatSymbols().apply {
+        groupingSeparator = ','
+        decimalSeparator = '.'
+    }
+    return DecimalFormat("#,###", symbols).format(this)
 }
