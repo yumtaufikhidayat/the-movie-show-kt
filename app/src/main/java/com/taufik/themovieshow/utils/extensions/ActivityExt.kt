c7efc07d4e22b6704533f81edae544168ee77439
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-08-21 06:02:53 +0700

    Refactor: Add `withSuccess` parameter to `refreshActivity`
    The `refreshActivity` extension function in `ActivityExt.kt` now accepts an optional `withSuccess` boolean parameter.
    When `withSuccess` is true, it adds an extra to the intent to indicate a successful language change.

--- app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
@@ -1,11 +1,16 @@
 package com.taufik.themovieshow.utils.extensions
 
 import android.app.Activity
+import com.taufik.themovieshow.ui.language.bottomsheet.LanguageBottomSheetDialog.Companion.SUCCESS_CHANGE_LANGUAGE
 
-fun Activity.refreshActivity() {
-    val intent = intent
+fun Activity.refreshActivity(withSuccess: Boolean = false) {
+    val newIntent = intent.apply {
+        if (withSuccess) {
+            putExtra(SUCCESS_CHANGE_LANGUAGE, true)
+        }
+    }
     finish()
     overridePendingTransition(0, 0)
-    startActivity(intent)
+    startActivity(newIntent)
     overridePendingTransition(0, 0)
 }
