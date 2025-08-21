eaab971374e8b877069255f5abba1853ba3dc6ad
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-08-21 08:32:18 +0700

    Refactor: Improve `refreshActivity` to restart the current activity correctly

--- app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
@@ -1,16 +1,17 @@
 package com.taufik.themovieshow.utils.extensions
 
 import android.app.Activity
+import android.content.Intent
 import com.taufik.themovieshow.ui.language.bottomsheet.LanguageBottomSheetDialog.Companion.SUCCESS_CHANGE_LANGUAGE
 
 fun Activity.refreshActivity(withSuccess: Boolean = false) {
-    val newIntent = intent.apply {
+    val newIntent = Intent(this, this::class.java).apply {
         if (withSuccess) {
             putExtra(SUCCESS_CHANGE_LANGUAGE, true)
         }
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
     }
-    finish()
-    overridePendingTransition(0, 0)
     startActivity(newIntent)
+    finish()
     overridePendingTransition(0, 0)
 }
