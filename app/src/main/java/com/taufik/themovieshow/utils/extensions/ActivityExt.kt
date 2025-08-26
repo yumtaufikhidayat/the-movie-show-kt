3d27082f4c4e0be3efe51afa5ec96001392bb5e2
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-08-26 16:10:42 +0700

    Refactor: Simplify `refreshActivity` extension
    The `refreshActivity` extension function in `ActivityExt.kt` has been simplified.
    The `withSuccess` parameter and the logic to put `SUCCESS_CHANGE_LANGUAGE` as an extra in the intent have been removed.
    The function now directly creates a new intent and starts the activity without any conditional extras.

--- app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
@@ -4,11 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import com.taufik.themovieshow.ui.language.bottomsheet.LanguageBottomSheetDialog.Companion.SUCCESS_CHANGE_LANGUAGE
 
-fun Activity.refreshActivity(withSuccess: Boolean = false) {
+fun Activity.refreshActivity() {
     val newIntent = Intent(this, this::class.java).apply {
-        if (withSuccess) {
-            putExtra(SUCCESS_CHANGE_LANGUAGE, true)
-        }
         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
     }
     startActivity(newIntent)
