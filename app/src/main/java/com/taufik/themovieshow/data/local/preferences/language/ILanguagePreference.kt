e40af4edd6fb2b3496c1ef18b91168015e1d32c6
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-08-28 16:10:15 +0700

    Refactor: Enhance `ILanguagePreference` interface
    The `ILanguagePreference` interface has been updated to provide more control and information regarding language settings:
    
    - The `setLanguage` function now accepts an additional `isChanged` boolean parameter to indicate if the language has been actively changed.
    - A new function `getLanguageChangedMessage()` has been added to return a `Flow<Boolean>` that emits whether the language has been changed.
    - A new suspend function `setLanguageChangedMessage(isChanged: Boolean)` has been added to allow setting the language changed status.

--- app/src/main/java/com/taufik/themovieshow/data/local/preferences/language/ILanguagePreference.kt
+++ app/src/main/java/com/taufik/themovieshow/data/local/preferences/language/ILanguagePreference.kt
@@ -3,7 +3,9 @@ package com.taufik.themovieshow.data.local.preferences.language
 import kotlinx.coroutines.flow.Flow
 
 interface ILanguagePreference {
-    suspend fun setLanguage(language: String)
-    suspend fun getLanguage(): String
     val languageFlow: Flow<String>
+    suspend fun setLanguage(language: String, isChanged: Boolean)
+    suspend fun getLanguage(): String
+    fun getLanguageChangedMessage(): Flow<Boolean>
+    suspend fun setLanguageChangedMessage(isChanged: Boolean)
 }
