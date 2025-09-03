7b52a2f4665c9dbdc650fa84b4ddd856149e4584
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-09-03 13:18:16 +0700

    Refactor: Standardize `Context` parameter name in `updateLocale`
    The `Context` parameter in the `updateLocale` function within `ContextUtils.kt` has been renamed from `c` to `mContext`. This change improves code readability and consistency.

--- app/src/main/java/com/taufik/themovieshow/utils/language/ContextUtils.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/language/ContextUtils.kt
@@ -9,9 +9,9 @@ import java.util.Locale
 
 class ContextUtils(base: Context) : ContextWrapper(base) {
     companion object {
-        fun updateLocale(c: Context, newLocale: Locale): ContextWrapper {
+        fun updateLocale(mContext: Context, newLocale: Locale): ContextWrapper {
             Locale.setDefault(newLocale)
-            val config = Configuration(c.resources.configuration)
+            val config = Configuration(mContext.resources.configuration)
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 config.setLocale(newLocale)
@@ -20,11 +20,10 @@ class ContextUtils(base: Context) : ContextWrapper(base) {
                 config.setLocale(newLocale)
             }
 
-            // 👇 Tambahan krusial: update default resource configuration
             @Suppress("DEPRECATION")
-            c.resources.updateConfiguration(config, c.resources.displayMetrics)
+            mContext.resources.updateConfiguration(config, mContext.resources.displayMetrics)
 
-            val localizedContext = c.createConfigurationContext(config)
+            val localizedContext = mContext.createConfigurationContext(config)
             return ContextUtils(localizedContext)
         }
     }
