c1c77e62b9261b5cc9115eeba8d078a030a1a803
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-09-06 19:12:15 +0700

    Refactor: Remove unused import

--- app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/extensions/ActivityExt.kt
@@ -2,7 +2,6 @@ package com.taufik.themovieshow.utils.extensions
 
 import android.app.Activity
 import android.content.Intent
-import com.taufik.themovieshow.ui.language.bottomsheet.LanguageBottomSheetDialog.Companion.SUCCESS_CHANGE_LANGUAGE
 
 fun Activity.refreshActivity() {
     val newIntent = Intent(this, this::class.java).apply {
