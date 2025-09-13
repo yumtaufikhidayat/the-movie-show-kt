39631ce63b8ce425b1b9f95d46dc46302fa1fe3e
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-09-13 13:40:52 +0700

    Feat: Add passphrase encryption for secure data storage
    - If a passphrase does not exist in `secureDataStore`, generate a new one, encrypt it, and store it.
    - This ensures sensitive data is protected.

--- app/src/main/java/com/App.kt
+++ app/src/main/java/com/App.kt
@@ -2,11 +2,17 @@ package com
 
 import android.app.Application
 import android.content.Context
+import com.taufik.themovieshow.utils.extensions.secureDataStore
+import com.taufik.themovieshow.utils.helper.SecureEncryptedKey.PASSPHRASE
+import com.taufik.themovieshow.utils.helper.SecureEncryptedKey.encryptAndStorePassphrase
 import com.taufik.themovieshow.utils.language.ContextUtils
 import com.taufik.themovieshow.utils.language.LANGUAGE
 import com.taufik.themovieshow.utils.language.LanguageCache
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import java.util.Locale
+import java.util.UUID
 
 @HiltAndroidApp
 class App : Application() {
@@ -22,5 +28,13 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         System.loadLibrary("sqlcipher")
+
+        runBlocking {
+            val passphraseExists = secureDataStore.data.first()[PASSPHRASE] != null
+            if (!passphraseExists) {
+                val rawPassphrase = UUID.randomUUID().toString()
+                encryptAndStorePassphrase(rawPassphrase)
+            }
+        }
     }
 }
