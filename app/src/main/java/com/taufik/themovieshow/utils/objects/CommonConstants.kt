354a9bda2bb5368a9f9c6eaa420fcd2a8d9c66a0
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-09-23 06:48:56 +0700

    Refactor: Add KEY_STORE_ANDROID constant
    - A new constant `KEY_STORE_ANDROID` with the value "AndroidKeyStore" has been added to the `CommonConstants.kt` file.

--- app/src/main/java/com/taufik/themovieshow/utils/objects/CommonConstants.kt
+++ app/src/main/java/com/taufik/themovieshow/utils/objects/CommonConstants.kt
@@ -3,9 +3,12 @@ package com.taufik.themovieshow.utils.objects
 import com.taufik.themovieshow.BuildConfig
 
 object CommonConstants {
+    /* Paging */
     const val STARTING_PAGE_INDEX = 1
     const val LOAD_PER_PAGE = 5
     const val LOAD_MAX_PER_PAGE = 25
+
+    /* Table & Database */
     const val TABLE_NAME_FAVORITE_MOVIE_ENTITY = "favorite_movie"
     const val COLUMN_NAME_POSTER = "poster"
     const val COLUMN_NAME_TITLE = "title"
@@ -15,13 +18,17 @@ object CommonConstants {
     const val TABLE_NAME_FAVORITE_TV_SHOW_ENTITY = "favorite_tv_show"
     const val DB_NAME = "TheMovieShowDB"
     const val DB_VERSION = 4
+    const val ENCRYPTED_DB_PASSPHRASE = BuildConfig.ENCRYPTED_DB_PASSPHRASE
     const val QUERY_API_KEY = "api_key"
     const val QUERY_Q = "query"
     const val QUERY_MOVIE_ID = "movie_id"
     const val QUERY_TV_SHOW_ID = "tv_id"
     const val QUERY_APPEND_TO_RESPONSE = "append_to_response"
+
+    /* API Key*/
     const val API_KEY = BuildConfig.API_KEY
-    const val ENCRYPTED_DB_PASSPHRASE = BuildConfig.ENCRYPTED_DB_PASSPHRASE
+
+    /* Profile */
     const val LINKEDIN = "linkedIn"
     const val GOOGLE_PLAY = "google_play"
     const val GITHUB = "github"
@@ -31,8 +38,9 @@ object CommonConstants {
     const val GITHUB_URL_LINK = "https://github.com/yumtaufikhidayat/the-movie-show-kt"
     const val EMAIL_ADDRESS = "yumtaufikhidayat@gmail.com"
 
-    /* Key for Secure Preferences */
+    /* Secure Preferences */
     const val KEY_SECURE_PREFS = "secure_prefs"
     const val KEY_PASSPHRASE = "db_passphrase"
     const val KEY_IV = "db_iv"
+    const val KEY_STORE_ANDROID = "AndroidKeyStore"
 }
