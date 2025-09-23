0fbf2f167c396b0354d69188ad5b44f088932489
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-09-23 14:02:37 +0700

    Refactor: Update PASSPHRASE import path
    - The import path for `PASSPHRASE` has been updated from `com.taufik.themovieshow.utils.helper.SecureEncryptedKey.PASSPHRASE` to `com.taufik.themovieshow.utils.objects.PreferencesKey.PASSPHRASE` in `App.kt`.

--- app/src/main/java/com/App.kt
+++ app/src/main/java/com/App.kt
@@ -3,11 +3,11 @@ package com
 import android.app.Application
 import android.content.Context
 import com.taufik.themovieshow.utils.extensions.secureDataStore
-import com.taufik.themovieshow.utils.helper.SecureEncryptedKey.PASSPHRASE
 import com.taufik.themovieshow.utils.helper.SecureEncryptedKey.encryptAndStorePassphrase
 import com.taufik.themovieshow.utils.language.ContextUtils
 import com.taufik.themovieshow.utils.language.LANGUAGE
 import com.taufik.themovieshow.utils.language.LanguageCache
+import com.taufik.themovieshow.utils.objects.PreferencesKey.PASSPHRASE
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
